37857a3bbf03ca00f9623f54cb1c811be84e93eb
Anshikagupta421 <guptaanshika76220@gmail.com>
2026-06-01 01:07:15 +0530

    web

---
+++ JS/dom.js
@@ -0,0 +1,16 @@
+ function show(){
+    let name=document.getElementById("myName").valueOf;
+    //document.getElementById ("display").innerHTML=name
+    alert(name);
+ }
+ //dom on 11th march
+ function changeColor(){
+    document.getElementById('text').style.color="red";
+
+ }
+ //colorchange on mouse over
+ //get the element by its ID us
+ const myElement = document.getElementById("text");
+
+ //ADD event listener for mouseover
+ myElement.addEventListener("mouseover",function())
